fb93c5152c8f64452c8e43797cc55f9544e56e88
yannick <yannickmacken@gmail.com>
2023-04-24 18:54:06 +0200

    image scrape from wiki

---
+++ write_images.py
@@ -0,0 +1,64 @@
+import os
+from io import BytesIO
+
+from supabase import create_client
+from dotenv import load_dotenv
+import requests
+from PIL import Image
+
+
+load_dotenv()
+
+
+supabase_client = create_client(os.getenv("SUPABASE_URL"), os.getenv("SUPABASE_KEY"))
+
+
+def get_animal_image(animal_name):
+    # format the query URL with the animal name
+    query_url = f"https://en.wikipedia.org/w/api.php?action=query&format=json&prop=pageimages&titles={animal_name}&pithumbsize=500"
+
+    # make the API request
+    response = requests.get(query_url).json()
+
+    # extract the image URL from the response
+    pages = response['query']['pages']
+    page_id = next(iter(pages))
+    image_url = pages[page_id]['thumbnail']['source']
+
+    # download and crop the image
+    response = requests.get(image_url)
+    image = Image.open(BytesIO(response.content))
+    image_width, image_height = image.size
+    aspect_ratio = image_width / image_height
+    target_aspect_ratio = 1
+    if target_aspect_ratio < aspect_ratio:
+        # crop the image horizontally
+        crop_width = int(image_height * target_aspect_ratio)
+        crop_height = image_height
+        x0 = (image_width - crop_width) // 2
+        y0 = 0
+    else:
+        # crop the image vertically
+        crop_width = image_width
+        crop_height = int(image_width / target_aspect_ratio)
+        x0 = 0
+        y0 = (image_height - crop_height) // 2
+    image = image.crop((x0, y0, x0 + crop_width, y0 + crop_height))
+    image_width, image_height = image.size
+    cover = image.resize((min(1000, image_width), min(1000, image_height)))
+    thumbnail = image.resize((min(150, image_width), min(150, image_height)))
+
+    # save the cropped image
+    file_path = os.path.join(os.getcwd() + "\\images\\", "test_cover.jpg")
+    cover.save(file_path)
+    file_path = os.path.join(os.getcwd() + "\\images\\", "test_thumbnail.jpg")
+    thumbnail.save(file_path)
+
+    return cover, thumbnail
+
+
+if __name__ == "__main__":
+
+    # example usage
+    animal_name = "lion"
+    cover, thumbnail = get_animal_image(animal_name)
