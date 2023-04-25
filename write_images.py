3e5b3eaa38f5a2d6697fb46663a87c3403d0ff6b
yannick <yannickmacken@gmail.com>
2023-04-25 12:27:47 +0200

    Update write_images.py

--- write_images.py
+++ write_images.py
@@ -2,6 +2,7 @@ import asyncio
 import os
 from io import BytesIO
 from typing import Tuple
+from urllib import request
 from urllib.parse import urlencode, quote
 
 import aiohttp
@@ -22,8 +23,8 @@ def adjust_image(image_url: str) -> Tuple[Image, Image]:
     """Adjust image at given url to specified sizes."""
 
     # download the image
-    response = requests.get(image_url)
-    image = Image.open(BytesIO(response.content))
+    r = request.urlopen(image_url)
+    image = Image.open(r)
     image_width, image_height = image.size
     aspect_ratio = image_width / image_height
     target_aspect_ratio = 1
@@ -45,19 +46,13 @@ def adjust_image(image_url: str) -> Tuple[Image, Image]:
     # Define large image and thumbnail
     image = image.crop((x0, y0, x0 + crop_width, y0 + crop_height))
     image_width, image_height = image.size
-    cover = image.resize((min(1000, image_width), min(1000, image_height)))
+    cover = image.resize((min(800, image_width), min(800, image_height)))
     thumbnail = image.resize((min(150, image_width), min(150, image_height)))
-
-    # # save the cropped image
-    # file_path = os.path.join(os.getcwd() + "\\images\\", animal_name + "_cover.jpg")
-    # cover.save(file_path)
-    # file_path = os.path.join(os.getcwd() + "\\images\\", animal_name + "_thumbnail.jpg")
-    # thumbnail.save(file_path)
 
     return cover, thumbnail
 
 
-def get_file_info(filename: str):
+def get_file_info(filename: str) -> Tuple[str, str, int, int]:
     """Get file infor for a given wiki image file. Returns url, license."""
 
     api_url = "https://commons.wikimedia.org/w/api.php"
@@ -68,7 +63,7 @@ def get_file_info(filename: str):
         "format": "json",
         "prop": "imageinfo",
         "titles": filename,
-        "iiprop": "url|extmetadata"
+        "iiprop": "url|extmetadata|dimensions"
     }
 
     response = requests.get(api_url, params=params)
@@ -80,27 +75,16 @@ def get_file_info(filename: str):
 
     original_url = image_info["url"]
     license = image_info["extmetadata"]["LicenseShortName"]["value"]
+    width = image_info["width"]
+    height = image_info["height"]
 
-    return original_url, license
+    return original_url, license, width, height
 
 
-async def get_animal_image_url_async(animal_name):
+def get_animal_image_url(animal_name: str):
     """Asynchronously send a request for an animal image to the wiki commons API.
     Limits the results to only images with a creative commons license, and with
     squarish proportions."""
-
-    print(animal_name)
-
-
-    #
-    # base_url = "https://commons.wikimedia.org/w/api.php"
-    # license = "&haslicense=attribution-same-license"
-    # other = "&prop=imageinfo&iiprop=url|size&format=json&gsrlimit=10&iiurlwidth=500&iiurlheight=500&iiurlparam=thumb|square&redirects=1"
-    #
-    # url = f"{base_url}?action=query&generator=search&gsrsearch={animal_name}"
-    # url = f"https://commons.wikimedia.org/w/api.php?action=query&iiprop=extmetadata&generator=images&prop=imageinfo&redirects=1&generator=search&gsrsearch={animal_name}&iiprop=timestamp|user|userid|comment|canonicaltitle|url|size|dimensions|sha1|mime|thumbmime|mediatype|bitdepth"
-    # print(url)
-    #
 
     # Search query for images with the animal name
     base_url = "https://commons.wikimedia.org/w/api.php"
@@ -112,7 +96,7 @@ async def get_animal_image_url_async(animal_name):
         'prop': 'imageinfo',
         'srnamespace': '6',
         'format': 'json',
-        'iiprop': 'timestamp|user|userid|comment|canonicaltitle|url|size|dimensions|sha1|mime|thumbmime|mediatype|bitdepth|extmetadata'
+        # 'iiprop': 'timestamp|user|userid|comment|canonicaltitle|url|size|dimensions|sha1|mime|thumbmime|mediatype|bitdepth|extmetadata'
     }
     headers = {
         'Content-Type': 'application/json;charset=UTF-8'}
@@ -123,47 +107,45 @@ async def get_animal_image_url_async(animal_name):
     # Get the first image with a CC license and squarish proportions
     img_url = None
     for img_result in response_dict["query"]["search"]:
-        img_url, license = get_file_info(img_result["title"])
+        title = img_result["title"]
+        if not title.startswith("File:") or not "jpg" in title:
+            continue
+        temp_url, license, width, height = get_file_info(title)
         if not "CC" in license and not "Public Domain" in license:
             continue
-        print("url", img_url)
+        img_url = temp_url
+        print("url", img_url, license, width, height)
         break
 
     if not img_url:
         return None
 
-    async with aiohttp.ClientSession() as session:
-        async with session.get(img_url) as response:
-            response = await response.json()
-    print(response)
-    for page_id, page_info in response["query"]["pages"].items():
-        if "imageinfo" in page_info:
-            image_info = page_info["imageinfo"][0]
-            width = image_info["width"]
-            height = image_info["height"]
-            if (
-                    1 <= width / height <= 1.2 or 1 <= height / width <= 1.2) and width >= 300:
-                return image_info["url"]
+    # Adjust the image to the desired sizes
+    cover, thumbnail = adjust_image(img_url)
 
-    return None
+    # save the cropped image
+    file_path = os.path.join(os.getcwd() + "\\images\\", animal_name + "_cover.jpg")
+    cover.save(file_path)
+    file_path = os.path.join(os.getcwd() + "\\images\\", animal_name + "_thumbnail.jpg")
+    thumbnail.save(file_path)
+
+    return img_url
 
 
-async def main():
+def main():
     """Main function."""
-    species = supabase_client.from_("species_view").select("*").limit(5)
+    species = supabase_client.from_("species_view").select("*").limit(20)
     result = species.execute()
 
     tasks = []
     for record in result.data:
 
         # Get thumbnail and cover image
-        animal_name = record.get("common_name") or record.get("genus") + " " + record.get("species")
-        tasks.append(get_animal_image_url_async(animal_name))
-        # cover, thumbnail = get_animal_image(animal_name)
-
-    return await asyncio.gather(*tasks)
+        binomial = record.get("genus") + " " + record.get("species")
+        animal_name = record.get("common_name") or binomial
+        url = get_animal_image_url(animal_name)
+        print(url)
 
 
 if __name__ == "__main__":
-    results = asyncio.run(main())
-    print(results)
+    main()
